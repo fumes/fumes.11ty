27cde1682beab5085db0aa5b649c08e17b3a4c4f
toybreaker <rokmabox@gmail.com>
2020-03-30 23:57:51 +0800

    installed responsive images from markdown to picture srcset with liquid template

--- .eleventy.js
+++ .eleventy.js
@@ -1,39 +1,39 @@
-const pluginSass = require('eleventy-plugin-sass');
+const pluginSass = require("eleventy-plugin-sass");
 
 module.exports = function(eleventyConfig) {
   // sass
   eleventyConfig.addPlugin(pluginSass, {
-    watch: 'src/scss/*.scss'
+    watch: "src/scss/*.scss" // "single quotes" here do NO work ?!
   });
 
   // pass some assets right through
-  eleventyConfig.addPassthroughCopy('src/assets');
+  eleventyConfig.addPassthroughCopy("src/assets");
 
   // Aliases are in relation to the _includes folder
-  eleventyConfig.addLayoutAlias('categories', 'layouts/categories.html');
-  eleventyConfig.addLayoutAlias('default', 'layouts/default.html');
-  eleventyConfig.addLayoutAlias('home', 'layouts/home.html');
-  eleventyConfig.addLayoutAlias('images_author', 'layouts/images_author.html');
-  eleventyConfig.addLayoutAlias('page', 'layouts/page.html');
-  eleventyConfig.addLayoutAlias('photographer', 'layouts/photographer.html');
-  eleventyConfig.addLayoutAlias('post_index_tag', 'layouts/post_index_tag.html');
-  eleventyConfig.addLayoutAlias('post', 'layouts/post.html');
-  eleventyConfig.addLayoutAlias('text_author', 'layouts/text_author.html');
+  eleventyConfig.addLayoutAlias("categories", "layouts/categories.html");
+  eleventyConfig.addLayoutAlias("default", "layouts/default.html");
+  eleventyConfig.addLayoutAlias("home", "layouts/home.html");
+  eleventyConfig.addLayoutAlias("images_author", "layouts/images_author.html");
+  eleventyConfig.addLayoutAlias("page", "layouts/page.html");
+  eleventyConfig.addLayoutAlias("photographer", "layouts/photographer.html");
+  eleventyConfig.addLayoutAlias("post_index_tag", "layouts/post_index_tag.html");
+  eleventyConfig.addLayoutAlias("post", "layouts/post.html");
+  eleventyConfig.addLayoutAlias("text_author", "layouts/text_author.html");
 
   eleventyConfig.setLiquidOptions({
     dynamicPartials: false,
     root: [
-      '_includes',
-      '.'
+      "_includes",
+      "."
     ]
   });
 
   return {
     dir: {
-      input: 'src',
-      includes: '_includes',
-      data: '._data',
-      output: '_site'
+      input: "src",
+      includes: "_includes",
+      data: "_data",
+      output: "_site"
     }
   };
 }
---
+++ src/articles/skin-works.md
@@ -0,0 +1,44 @@
+---
+title: Skin Works
+text_author:
+photographer: rokma
+curator: rokma
+date: 2019-05-19
+category: art
+tags:
+- beauty
+- celebration
+- culture
+- erotic
+- fun
+- glamour
+- life
+- lifestyle
+- personal-works
+- photography
+- simplicity
+- shape
+- Thailand
+- women
+- visual
+- fine-art-prints
+license: CC_BY_NC_ND
+thumb: skin-works.jpg
+excerpt: "Skin Works is a series of fine-art black-and-white prints by Rokma. While your summer might look pink, you can feel black and white. Shades of grey smoothly attract. Not surviving alone in the jungle, we are moved to seek contact. See with our hands. Feel the importance. Acknowledge. Culturally gravitating around beauty, skin works exploring the vibe, no light needed."
+offwhite: true # true or empty
+image: /assets/p/articles/art/skin-works/skin-works.jpg
+---
+
+Skin Works is a series of fine-art black-and-white prints by Rokma.
+
+While your summer might look pink, you can feel black and white. Shades of grey smoothly attract. Not surviving alone in the jungle, we are moved to seek contact. See with our hands. Feel the importance. Acknowledge. Gravitating culturally around beauty, skin works exploring the vibe, no light needed.
+
+{% include partials/image.html, name: "" %}
+{% include partials/image.html, name: "-02", dida: "ddd" %}
+{% include partials/image.html, name: "-03" %}
+{% include partials/image.html, name: "-04" %}
+{% include partials/image.html, name: "-05" %}
+{% include partials/image.html, name: "-06" %}
+{% include partials/image.html, name: "-07" %}
+{% include partials/image.html, name: "-08" %}
+{% include partials/image.html, name: "-09" %}
