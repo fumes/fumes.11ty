eb33b24ba71d038777a6e08e899517f8f5cde96d
toybreaker <toybreaker@users.noreply.github.com>
2020-06-25 12:32:15 +0800

    added convert gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 'use strict';
-// GULP tasks to work images.
+// GULP tasks to SIZE AND RENAME images.
 var changeCase   = require('change-case');
 var gulp         = require('gulp');
 var rename       = require('gulp-rename');
 var responsive   = require('gulp-responsive');
 var del          = require('del');
+
+var imagesConvert = require('gulp-images-convert');
+
+gulp.task('convert', function () {
+  return gulp.src('./src/uploads/_images_converted/**/*.jpg')
+      .pipe(imagesConvert({targetType: 'png'}))
+      .pipe(rename({extname: ".png"}))
+      .pipe(gulp.dest('./src/uploads/_images_to_convert/'));
+})
 
 // WHAT: adds folder name + index, lowercasing the original file name.
 // starts from 10, to avoid single digit no. to the first 9 jpgs
